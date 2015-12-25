fe04a0d453aff277cbd346c4e7be70e47a3b5cf2
Eden Le Comte <Eden Le Comte>
2015-12-26 11:11:51 +1300

    Initial push

---
+++ www/js/maps/markers.js
@@ -0,0 +1,78 @@
+//define all divIcon markers
+//icons are referenced in markers.css
+
+//default marker
+	var markerDefault = L.divIcon({
+      	iconSize: ([35, 50]),
+      	iconAnchor: ([15, 42]),
+      	popupAnchor: ([0, -40]),
+		className: 'marker-default'
+	});
+	
+//CCC marker
+	var markerCCCwhite = L.divIcon({
+      	iconSize: ([35, 50]),
+      	iconAnchor: ([15, 42]),
+      	popupAnchor: ([0, -40]),
+		className: 'marker-cccwhite'
+	});
+        
+	
+//parking markers
+
+//unrestricted parking
+	var markerParking = L.divIcon({
+      	iconSize: ([35, 50]),
+      	iconAnchor: ([15, 42]),
+      	popupAnchor: ([0, -40]),
+		className: 'marker-parking'
+	});
+    
+//60 minutes
+	var markerP60 = L.divIcon({
+      	iconSize: ([35, 50]),
+      	iconAnchor: ([15, 42]),
+      	popupAnchor: ([0, -40]),
+		className: 'marker-p60'
+	});
+    
+//120 minutes		
+	var markerP120 = L.divIcon({
+      	iconSize: ([35, 50]),
+      	iconAnchor: ([15, 42]),
+      	popupAnchor: ([0, -40]),
+		className: 'marker-p120'
+	});
+				
+//Mobility parking
+	var markerPmobility = L.divIcon({
+      	iconSize: ([35, 50]),
+      	iconAnchor: ([15, 42]),
+      	popupAnchor: ([0, -40]),
+		className: 'marker-pMobility'
+	});
+					
+//Motorcycle parking
+	var markerPmotorcycle = L.divIcon({
+      	iconSize: ([35, 50]),
+      	iconAnchor: ([15, 42]),
+      	popupAnchor: ([0, -40]),
+		className: 'marker-pMotorcycle'
+	});
+							
+//Taxi stand parking
+	var markerPtaxi = L.divIcon({
+      	iconSize: ([35, 50]),
+      	iconAnchor: ([15, 42]),
+      	popupAnchor: ([0, -40]),
+		className: 'marker-pTaxi'
+	});
+
+//Park Smart parking
+	var markerParksmart = L.divIcon({
+      	iconSize: ([35, 50]),
+      	iconAnchor: ([15, 42]),
+      	popupAnchor: ([0, -40]),
+		className: 'marker-parksmart'
+	});
+																		
